e1dbb513c0995bb41fee6d86d1ed92fb3d49714a
ceres r <ceres r>
2016-06-18 21:18:52 -0300

    :rocket: Add model
    Adiciona o modelo de dados.

---
+++ MangaReminder/MangeReminder.Model/Entities/Manga.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MangaReminder.Model
+{
+    public class Manga
+    {
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ MangaReminder/MangeReminder.Model/Exceptions/CorruptedMangaException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MangaReminder.Model.Exceptions
+{
+    public class CorruptedMangaException : Exception
+    {
+        public CorruptedMangaException(string corruptedField) : base("Details extracted from Panini web page are corrupted. Field: " + corruptedField)
+        {
+
+        }
+    }
+}

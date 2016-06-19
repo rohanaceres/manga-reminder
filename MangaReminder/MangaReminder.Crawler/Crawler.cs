cfd15316526c03d6729f4a64ce7ff6fe84feadc6
ceres r <ceres r>
2016-06-18 21:29:32 -0300

    :memo: Documentação

--- MangaReminder/MangaReminder.Crawler/Crawler.cs
+++ MangaReminder/MangaReminder.Crawler/Crawler.cs
@@ -7,22 +7,41 @@ using System.Globalization;
 
 namespace MangaReminder.Crawler
 {
-    public class Crawler
+    /// <summary>
+    /// Crawler para o site http://www.paninicomics.com.br/web/guest/home
+    /// </summary>
+    public class MangaCrawler
     {
+        /// <summary>
+        /// Especifica o timeout do download da pagina HTML.
+        /// </summary>
         internal const int HttpTimeOut = 30000;
+        /// <summary>
+        /// URL do mangá a ser crawleado.
+        /// </summary>
         Uri Uri { get; set; }
 
-        public Crawler(string uri)
+        /// <summary>
+        /// Define a propriedade <see cref="Uri"/> de acordo com a
+        /// string recebida como parâmetro.
+        /// </summary>
+        /// <param name="url">URL do site.</param>
+        public MangaCrawler(string url)
         {
-            this.Uri = new Uri(uri);
+            this.Uri = new Uri(url);
         }
 
+        /// <summary>
+        /// Abre a página do mangá e busca pela ultima edição
+        /// lançada.
+        /// </summary>
+        /// <returns>A ultima edição lançada.</returns>
         public Manga GetLast()
         {
-            Uri newUri = GetMangaDetailsUri();
+            Uri newUri = GetLastMangaDetailsUri();
 
             IConnection connection = NSoupClient.Connect(newUri.AbsoluteUri);
-            connection.Timeout(Crawler.HttpTimeOut);
+            connection.Timeout(MangaCrawler.HttpTimeOut);
             Document document = connection.Get();
 
             Manga self = new Manga();
@@ -33,10 +52,15 @@ namespace MangaReminder.Crawler
             return self;
         }
 
-        private Uri GetMangaDetailsUri()
+        /// <summary>
+        /// Retorna a URI de detalhes do ultimo mangá
+        /// lançado.
+        /// </summary>
+        /// <returns>A URI que contem os detalhos do ultimo mangá lançado.</returns>
+        private Uri GetLastMangaDetailsUri()
         {
             IConnection connection = NSoupClient.Connect(this.Uri.AbsoluteUri);
-            connection.Timeout(Crawler.HttpTimeOut);
+            connection.Timeout(MangaCrawler.HttpTimeOut);
             Document document = connection.Get();
 
             string redirect = document.GetElementsByClass("item").First
@@ -46,6 +70,12 @@ namespace MangaReminder.Crawler
 
             return new Uri(this.Uri, redirect);
         }
+        /// <summary>
+        /// De acordo com o HTML da página de detalhes do mangá,
+        /// esse método busca o preço do mangá.
+        /// </summary>
+        /// <param name="details">A página HTML de detalhes do mangá.</param>
+        /// <returns>O preço do mangá.</returns>
         private decimal GetMangaPriceFromDetails(Document details)
         {
             string priceStr = details.GetElementsByClass("price").First.Text().Split(' ')[8];
@@ -60,6 +90,12 @@ namespace MangaReminder.Crawler
 
             return price;
         }
+        /// <summary>
+        /// De acordo com o HTML da página de detalhes do mangá,
+        /// esse método busca a data de lançamento do mangá.
+        /// </summary>
+        /// <param name="details">A página HTML de detalhes do mangá.</param>
+        /// <returns>A data de lançamento do mangá.</returns>
         private DateTime GetMangaReleaseDateFromDetails(Document details)
         {
             string dateTimeStr = details.GetElementsByClass("price").First.Text().Split(' ')[5];
